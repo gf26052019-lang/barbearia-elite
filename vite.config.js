bcffe73faf04a183d606886fe2fede21b9ff196d
gf26052019-lang <gf26052019@gmail.com>
2025-11-08 15:45:10 -0300

    Add build configuration to Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
